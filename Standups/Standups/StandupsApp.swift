93b301a2a95b31282afe8aa63b05e911dc5a6b10
Nolin McFarland <nolin.mcfarland@icloud.com>
2023-09-24 20:59:23 -0400

    Initial commit

---
+++ Standups/Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Nolin McFarland on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
