8bffae6f2cc0d375b749d77b1c4f89f07f208d7c
Nolin McFarland <nolin.mcfarland@icloud.com>
2023-09-24 21:03:35 -0400

    Create stand ups list

---
+++ Standups/Standups/StandupsList.swift
@@ -0,0 +1,22 @@
+//
+//  StandupsList.swift
+//  Standups
+//
+//  Created by Nolin McFarland on 9/24/23.
+//
+
+import SwiftUI
+
+struct StandupsList: View {
+    var body: some View {
+        NavigationStack {
+            List {
+            }
+            .navigationTitle("Daily Standups")
+        }
+    }
+}
+
+#Preview {
+    StandupsList()
+}
